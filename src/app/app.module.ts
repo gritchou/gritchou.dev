f5ac909517d3c8ba417f4f6d766da00fc31fb461
gritchou <gritchou@gmail.com>
2020-03-12 17:36:16 +0100

    feat(about): create homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { FooterComponent } from './footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        FooterComponent,
+        AboutComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatCardModule,
+        FontAwesomeModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFilePdf } from '@fortawesome/free-regular-svg-icons';
+import { faEnvelopeSquare, faDownload, faFileDownload, faIdCard } from '@fortawesome/free-solid-svg-icons';
+
+const URLS = {
+    twitter: 'https://twitter.com/gritchou',
+    github: 'https://github.com/gritchou',
+    linkedin: 'https://www.linkedin.com/in/jfgrand/',
+    resume: 'assets/resume/jfgrand.pdf',
+}
+
+@Component({
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+    @HostBinding('class.mat-elevation-z6') elevation: boolean = true;
+    faLinkedin = faLinkedin;
+    faGithub = faGithub;
+    faTwitter = faTwitter;
+    faEnvelopeSquare = faEnvelopeSquare;
+    faIdCard = faIdCard;
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    openLink(target) {
+        window.open(URLS[target], '_blank');
+    }
+
+    sendMail() {
+        const link = document.createElement('a');
+        link.setAttribute('href', 'mailto:jfr.grand@gmail.com');
+        link.click();
+    }
+
+}
