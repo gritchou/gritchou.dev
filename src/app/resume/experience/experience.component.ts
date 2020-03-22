619bef2580fa57107b15ad91f6e96ef74d7fd34d
gritchou <gritchou@gmail.com>
2020-03-22 22:00:32 +0100

    feat(resume): update experience component to handle responsive

--- src/app/resume/experience/experience.component.ts
+++ src/app/resume/experience/experience.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
-import { Experience } from "../resume";
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+
+import { Experience } from '../resume';
 
 @Component({
-    selector: "app-experience",
-    templateUrl: "./experience.component.html",
-    styleUrls: ["./experience.component.scss"],
-    encapsulation: ViewEncapsulation.None
+	selector: 'app-experience',
+	templateUrl: './experience.component.html',
+	styleUrls: ['./experience.component.scss'],
+	encapsulation: ViewEncapsulation.None
 })
-export class ExperienceComponent implements OnInit {
-    @Input() experience: Experience;
-    constructor() {}
+export class ExperienceComponent {
+	@Input() experience!: Experience;
 
-    ngOnInit(): void {}
-
-    getSrc(): string {
-        return "./assets/" + this.experience.image;
-    }
+	getSrc(): string {
+		return `./assets/${this.experience.image}`;
+	}
 }
