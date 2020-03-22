57c6dd352686b3273a7df5e88329d7121c9aca68
gritchou <gritchou@gmail.com>
2020-03-22 23:39:01 +0100

    chore: rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,16 +21,16 @@ describe('AppComponent', () => {
 		expect(app).toBeTruthy();
 	});
 
-	it(`should have as title 'gritche'`, () => {
+	it(`should have as title 'gritchou.dev'`, () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		const app = fixture.componentInstance;
-		expect(app.title).toEqual('gritche');
+		expect(app.title).toEqual('gritchou.dev');
 	});
 
 	it('should render title', () => {
 		const fixture = TestBed.createComponent(AppComponent);
 		fixture.detectChanges();
 		const compiled = fixture.nativeElement;
-		expect(compiled.querySelector('.content span').textContent).toContain('gritche app is running!');
+		expect(compiled.querySelector('.content span').textContent).toContain('gritchou.dev app is running!');
 	});
 });
