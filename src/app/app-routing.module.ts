3a220c0d791a06e2e5dcb6da516cfd3af0d5539c
gritchou <gritchou@gmail.com>
2020-03-22 22:02:42 +0100

    chore: update rooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'about', pathMatch: 'full' },
