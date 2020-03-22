824ddd73e39a36afbebecd09f07ed1eb8ed07311
gritchou <gritchou@gmail.com>
2020-03-22 19:36:33 +0100

    feat(about): rework about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
-import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ResumeComponent } from './resume/resume.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ExperienceComponent } from './resume/experience/experience.component';
 import { SectionComponent } from './section/section.component';
+import { EducationComponent } from './resume/education/education.component';
+import { SkillComponent } from './resume/skill/skill.component';
 
 @NgModule({
 	declarations: [
@@ -23,6 +25,8 @@ import { SectionComponent } from './section/section.component';
 		ResumeComponent,
 		PortfolioComponent,
 		ExperienceComponent,
+		EducationComponent,
+		SkillComponent,
 		SectionComponent
 	],
 	imports: [
