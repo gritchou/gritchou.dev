682aa82d7f6bedaac109d69069727c0a48683c37
gritchou <gritchou@gmail.com>
2020-03-24 14:36:53 +0100

    feat(about): display first name

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-about',
 	templateUrl: './about.component.html',
 	styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
-
-	ngOnInit(): void {
-	}
-
-}
+export class AboutComponent { }
