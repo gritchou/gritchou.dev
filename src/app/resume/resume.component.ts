3ce976ca2376db0e76b4d8f85a07dd703438cef1
gritchou <gritchou@gmail.com>
2020-04-11 05:46:01 +0200

    chore(resume): use types

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 
 import { header, experiences, educations, skills } from '../../assets/resume/jfgrand.json';
 
+import { Header, Experience, Education, Skill } from './resume';
+
 @Component({
 	selector: 'app-resume',
 	templateUrl: './resume.component.html',
 	styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent {
-	experiences = experiences;
-	educations = educations;
-	skills = skills;
-	header = header;
+	experiences: Experience[] = experiences;
+	educations: Education[] = educations;
+	skills: Skill[] = skills;
+	header: Header = header;
 
 	openPdf() {
 		window.open('assets/resume/jfgrand.pdf', '_blank');
